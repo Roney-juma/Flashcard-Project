d3845a41d490c22625f63416cf29284bcc6a35bb
AbdiazizH <a.aziz69890@gmail.com>
2021-07-22 11:35:01 +0300

    added authentication

--- card/models.py
+++ card/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Unit(models.Model):
+    unit = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.unit
+
+
+class Card(models.Model):
+  title = models.CharField(max_length=144)
+  subject = models.ForeignKey(Unit,on_delete=models.CASCADE)
+  front_side = models.TextField()
+  back_side = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def save_card(self):
+    self.save()
+
+  def delete_card(self):
+    self.delete()
+
+  @classmethod
+  def search_cards(cls, title):
+    return cls.objects.filter(title__icontains=title).all()
+
+  @classmethod
+  def show_cards(cls):
+    cards = cls.objects.all()
+    return cards
+
+  def __str__(self):
+    return self.title
--- card/views.py
+++ card/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import  CreateUserForm
 # Create your views here.
+def home(request):
+ 
+    context = {}   
+    return render(request, 'flashcards/index.html', context)
+
+
+
+def registerPage(request):
+    form = CreateUserForm(request.POST)
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+          
+            messages.success(request, 'account was created for ' + username)
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'flashcards/register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password =request.POST.get('password')
+    
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('flashcard')
+        else:
+                messages.info(request,'Username or password is incrorrect')
+    context = {}
+    return render(request, 'flashcards/login.html', context)
+def logoutUser(request):
+	logout(request)
+	return redirect('home')
+def flashcards(request):
+    '''
+    Renders the flashcard app's flashcards.html template
+    '''
+    # Card_Set.objects.all() returns all cards in the database
+    # topic_query_set = Card_Set.objects.all().order_by('topic').filter(is_active = True)
+    context = {}   # Dictionary{} rendered for flashcards/flashcards.html
+    return render(request, 'flashcards/flashcard.html', context)
